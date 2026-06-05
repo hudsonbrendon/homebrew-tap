5663519b66046ffef175f97895d4d707ee0229b0
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2026-06-05 16:36:08 -0300

    fix: drop unnecessary verified url param (brew audit)

--- Casks/mister-companion.rb
+++ Casks/mister-companion.rb
@@ -2,8 +2,7 @@ cask "mister-companion" do
   version "0.2.0"
   sha256 "0e64f851d6af2ae438b73d7b2a4efda953981783610da82e24264032be8e882d"
 
-  url "https://github.com/hudsonbrendon/mister-companion/releases/download/v#{version}/MiSTer-Companion-#{version}-arm64.dmg",
-      verified: "github.com/hudsonbrendon/mister-companion/"
+  url "https://github.com/hudsonbrendon/mister-companion/releases/download/v#{version}/MiSTer-Companion-#{version}-arm64.dmg"
   name "MiSTer Companion"
   desc "Cross-platform desktop companion for MiSTer FPGA"
   homepage "https://github.com/hudsonbrendon/mister-companion"
